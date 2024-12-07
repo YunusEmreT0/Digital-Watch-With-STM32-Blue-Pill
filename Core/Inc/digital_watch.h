f362d364ca062e1d42569b9f6d2da45a34054040
Yunus Emre <yns-emre-34@hotmail.com>
2024-12-07 14:40:10 +0300

    Added Comments

--- Core/Inc/digital_watch.h
+++ Core/Inc/digital_watch.h
@@ -8,16 +8,19 @@
 #ifndef INC_DIGITAL_WATCH_H_
 #define INC_DIGITAL_WATCH_H_
 
+// Should be changed according to the pin that buzzer is connected
 #define ALARM_GPIO_PER GPIOA
 #define ALARM_GPIO_PIN GPIO_PIN_15
 
+// Should be changed according to the pins that buttons are connected
 #define BUTTON_C_GPIO_PIN GPIO_PIN_1
 #define BUTTON_L_GPIO_PIN GPIO_PIN_2
 #define BUTTON_A_GPIO_PIN GPIO_PIN_3
 
+// Delay time for blink effect that can be seen while setting a value
+// Lower value means faster blink
 #define BLINK_DELAY_TIME 300
 
-void Digital_Watch_Print_Value(uint8_t value);
 void Digital_Watch_Main(RTC_HandleTypeDef *hrtc);
 void Digital_Watch_Timekeeping_Mode(RTC_HandleTypeDef *hrtc);
 void Digital_Watch_Alarm_Mode(RTC_HandleTypeDef *hrtc);
@@ -26,5 +29,6 @@ void Digital_Watch_Setting_Mode();
 void Digital_Watch_Welcome();
 void Digital_Watch_AlarmEventCallback();
 void Digital_Watch_Button_Pressed(uint16_t GPIO_Pin);
+void Digital_Watch_Print_Value(uint8_t value);
 
 #endif /* INC_DIGITAL_WATCH_H_ */
--- Core/Src/digital_watch.c
+++ Core/Src/digital_watch.c
@@ -5,9 +5,22 @@
  *      Author: Yunus Emre TÜKEL
  */
 
-#include "digital_watch.h"
-#include "liquidcrystal_i2c.h"
 #include "stm32f1xx_hal.h"
+#include "digital_watch.h"
+
+/*
+ *  16x2 LCD screen is used in this project
+ *	liquidcrystal_i2c library files are taken from the following link
+ *	https://github.com/eziya/STM32_HAL_I2C_HD44780/blob/master/Src/
+ *	Simply add liquidcrystal_i2c.h and liquidcrystal_i2c.c files to your
+ * 		projects Core/Src/ and Core/Inc/ folders
+ *  In liquidcrystal_i2c.h file:
+ *		Change line 4 to 	#include “stm32f1xx_hal.h”
+ *		Change line 58 to 	#define DEVICE_ADDR (0x27 << 1)
+ *	For more details on this library:
+ *	https://microcontrollerslab.com/i2c-lcd-stm32-blue-pill-stm32cubeide/
+ */
+#include "liquidcrystal_i2c.h"
 
 const char *week_days[7] = {
 	"SU",
@@ -18,31 +31,37 @@ const char *week_days[7] = {
 	"FR",
 	"SA"};
 
+/*
+ *	Variables to hold current mode and setting.
+ *	This project has 4 modes and different amount of settings in different modes.
+ *	For example in alarm mode sett value can be 0, 1 or 2.
+ */
 static uint8_t mode = 0;
 static uint8_t sett = 0;
 
+//	Flags for button press
 static uint8_t button_c_pressed = 0;
 static uint8_t button_l_pressed = 0;
 static uint8_t button_a_pressed = 0;
 
+//	Variables used in clock setting mode
 static RTC_TimeTypeDef time_setting;
 static RTC_DateTypeDef date_setting;
 
+//	Variables used in alarm mode
 static RTC_AlarmTypeDef alarm;
 static volatile uint8_t alarm_triggered = 0;
 static uint32_t alarm_start_time = 0;
 
-void Digital_Watch_Print_Value(uint8_t value)
-{
-	char str[3];
-	if (value < 10)
-		HD44780_PrintStr("0");
-	itoa(value, str, 16);
-	HD44780_PrintStr(str);
-}
-
+/*
+ *	Main function for digital watch
+ *	Must be called in while loop in main.c
+ *	Checks for button presses, resets buzzer after 20 seconds and calls functions according to current mode value
+ */
 void Digital_Watch_Main(RTC_HandleTypeDef *hrtc)
 {
+	//	To reset buzzer after alarm is triggered
+	//	User should wait for 20 seconds or press button L to silence buzzer
 	if (alarm_triggered)
 	{
 		if ((HAL_GetTick() - alarm_start_time) >= 20000)
@@ -57,6 +76,9 @@ void Digital_Watch_Main(RTC_HandleTypeDef *hrtc)
 			button_l_pressed = 0;
 		}
 	}
+
+	//	Main job of button C is changing mode.
+	//	This part changes mode and makes adjustments before it.
 	if (button_c_pressed)
 	{
 		HD44780_Clear();
@@ -84,6 +106,8 @@ void Digital_Watch_Main(RTC_HandleTypeDef *hrtc)
 		button_a_pressed = 0;
 		sett = 0;
 	}
+
+	//	Button L changes setting in modes.
 	if (button_l_pressed)
 	{
 		sett++;
@@ -92,6 +116,8 @@ void Digital_Watch_Main(RTC_HandleTypeDef *hrtc)
 		button_l_pressed = 0;
 		button_a_pressed = 0;
 	}
+
+	//	Function calls according to mode
 	switch (mode)
 	{
 	case 0:
@@ -109,6 +135,10 @@ void Digital_Watch_Main(RTC_HandleTypeDef *hrtc)
 	}
 }
 
+/*
+ *	Function for timekeeping mode, default mode
+ *	Prints current time, date and weekday on screen
+ */
 void Digital_Watch_Timekeeping_Mode(RTC_HandleTypeDef *hrtc)
 {
 	RTC_TimeTypeDef time;
@@ -136,21 +166,30 @@ void Digital_Watch_Timekeeping_Mode(RTC_HandleTypeDef *hrtc)
 	HD44780_PrintStr(week_days[date.WeekDay]);
 }
 
+/*
+ *	Function for alarm mode
+ *	Prints current alarm time on screen
+ *	Allows to change currrent alarm by pressing button L
+ *	After pressing button L once hour on screen blinks and
+ *		by pressing button A hour value increases
+ *	Second press on button L allows to set minutes
+ *	Third press sets a new alarm and returns to default alarm mode screen
+ */
 void Digital_Watch_Alarm_Mode(RTC_HandleTypeDef *hrtc)
 {
 	HD44780_SetCursor(0, 0);
 	HD44780_PrintStr("Alarm");
 
-	HD44780_SetCursor(4, 1);
+	HD44780_SetCursor(5, 1);
 	Digital_Watch_Print_Value(alarm.AlarmTime.Hours);
 	HD44780_PrintStr(":");
 	Digital_Watch_Print_Value(alarm.AlarmTime.Minutes);
 
 	switch (sett)
 	{
-	case 1:
+	case 1: //	Set Hours
 		HAL_Delay(BLINK_DELAY_TIME);
-		HD44780_SetCursor(0, 1);
+		HD44780_SetCursor(5, 1);
 		HD44780_PrintStr("  ");
 		HAL_Delay(BLINK_DELAY_TIME);
 		if (button_a_pressed)
@@ -163,9 +202,9 @@ void Digital_Watch_Alarm_Mode(RTC_HandleTypeDef *hrtc)
 			button_a_pressed = 0;
 		}
 		break;
-	case 2:
+	case 2: //	Set Minutes
 		HAL_Delay(BLINK_DELAY_TIME);
-		HD44780_SetCursor(3, 1);
+		HD44780_SetCursor(8, 1);
 		HD44780_PrintStr("  ");
 		HAL_Delay(BLINK_DELAY_TIME);
 		if (button_a_pressed)
@@ -178,19 +217,34 @@ void Digital_Watch_Alarm_Mode(RTC_HandleTypeDef *hrtc)
 			button_a_pressed = 0;
 		}
 		break;
-	case 3:
+	case 3: //	Set Alarm and return to default
 		HAL_RTC_SetAlarm_IT(hrtc, &alarm, RTC_FORMAT_BCD);
 		sett = 0;
 		break;
 	}
 }
 
+/*
+ *	Will be added
+ */
 void Digital_Watch_Stopwatch_Mode()
 {
 	HD44780_SetCursor(0, 0);
 	HD44780_PrintStr("Stopwatch");
 }
 
+/*
+ *	Function for clock setting mode
+ *	Prints the time on screen
+ *	Value that will be set blinks
+ *	Button A increases the value by 1 (not applied to seconds)
+ *	Button L changes to a different value
+ *	If button C is pressed while setting weekday
+ *		new time and date is set and mode changed to timekeeping
+ *		(code for this is in Digital_Watch_Main() function)
+ *	If button C is pressed while setting a different value
+ *		clock setting mode cancels
+ */
 void Digital_Watch_Setting_Mode()
 {
 	HD44780_SetCursor(0, 0);
@@ -214,7 +268,7 @@ void Digital_Watch_Setting_Mode()
 	HAL_Delay(BLINK_DELAY_TIME);
 	switch (sett)
 	{
-	case 0:
+	case 0: //	Set Seconds
 		HD44780_SetCursor(6, 0);
 		HD44780_PrintStr("  ");
 		HAL_Delay(BLINK_DELAY_TIME);
@@ -224,7 +278,7 @@ void Digital_Watch_Setting_Mode()
 			button_a_pressed = 0;
 		}
 		break;
-	case 1:
+	case 1: //	Set Hours
 		HD44780_SetCursor(0, 0);
 		HD44780_PrintStr("  ");
 		HAL_Delay(BLINK_DELAY_TIME);
@@ -238,7 +292,7 @@ void Digital_Watch_Setting_Mode()
 			button_a_pressed = 0;
 		}
 		break;
-	case 2:
+	case 2: //	Set Minutes
 		HD44780_SetCursor(3, 0);
 		HD44780_PrintStr("  ");
 		HAL_Delay(BLINK_DELAY_TIME);
@@ -252,7 +306,7 @@ void Digital_Watch_Setting_Mode()
 			button_a_pressed = 0;
 		}
 		break;
-	case 3:
+	case 3: //	Set Date
 		HD44780_SetCursor(0, 1);
 		HD44780_PrintStr("  ");
 		HAL_Delay(BLINK_DELAY_TIME);
@@ -266,7 +320,7 @@ void Digital_Watch_Setting_Mode()
 			button_a_pressed = 0;
 		}
 		break;
-	case 4:
+	case 4: //	Set Month
 		HD44780_SetCursor(3, 1);
 		HD44780_PrintStr("  ");
 		HAL_Delay(BLINK_DELAY_TIME);
@@ -280,7 +334,7 @@ void Digital_Watch_Setting_Mode()
 			button_a_pressed = 0;
 		}
 		break;
-	case 5:
+	case 5: //	Set the Tens Place of the Year
 		HD44780_SetCursor(8, 1);
 		HD44780_PrintStr(" ");
 		HAL_Delay(BLINK_DELAY_TIME);
@@ -292,7 +346,7 @@ void Digital_Watch_Setting_Mode()
 			button_a_pressed = 0;
 		}
 		break;
-	case 6:
+	case 6: //	Set the Ones Place of the Year
 		HD44780_SetCursor(9, 1);
 		HD44780_PrintStr(" ");
 		HAL_Delay(BLINK_DELAY_TIME);
@@ -304,7 +358,7 @@ void Digital_Watch_Setting_Mode()
 			button_a_pressed = 0;
 		}
 		break;
-	case 7:
+	case 7: //	Set Weekday
 		HD44780_SetCursor(14, 1);
 		HD44780_PrintStr("  ");
 		HAL_Delay(BLINK_DELAY_TIME);
@@ -319,6 +373,10 @@ void Digital_Watch_Setting_Mode()
 	}
 }
 
+/*
+ *	Welcome function that is called after power is on for the first time
+ *	Must be called just before the while loop in main.c
+ */
 void Digital_Watch_Welcome()
 {
 	HD44780_Init(2);
@@ -330,7 +388,10 @@ void Digital_Watch_Welcome()
 	HAL_Delay(3000);
 	HD44780_Clear();
 }
-
+/*
+ *	Function that rings the alarm when interrupt happens
+ *	Must be called in HAL_RTC_AlarmAEventCallback() function in main.c
+ */
 void Digital_Watch_AlarmEventCallback()
 {
 	HAL_GPIO_WritePin(ALARM_GPIO_PER, ALARM_GPIO_PIN, GPIO_PIN_SET);
@@ -338,6 +399,10 @@ void Digital_Watch_AlarmEventCallback()
 	alarm_start_time = HAL_GetTick();
 }
 
+/*
+ *	Function that reads button presses
+ *	Must be called in HAL_GPIO_EXTI_Callback() function in main.c
+ */
 void Digital_Watch_Button_Pressed(uint16_t GPIO_Pin)
 {
 	switch (GPIO_Pin)
@@ -352,4 +417,14 @@ void Digital_Watch_Button_Pressed(uint16_t GPIO_Pin)
 		button_a_pressed = 1;
 		break;
 	}
+}
+
+// Helper function to print values on screen
+void Digital_Watch_Print_Value(uint8_t value)
+{
+	char str[3];
+	if (value < 10)
+		HD44780_PrintStr("0");
+	itoa(value, str, 16);
+	HD44780_PrintStr(str);
 }
